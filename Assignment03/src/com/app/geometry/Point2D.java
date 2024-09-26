132886085926acd15d7d129729954dd30d140a6e
Juili Vyas <juilivyas01.nf@gmail.com>
2024-09-26 18:32:55 +0530

    Assignment 3 Q3 completed

---
+++ Assignment03/src/com/app/geometry/Point2D.java
@@ -0,0 +1,40 @@
+package com.app.geometry;
+
+public class Point2D {
+	double x;
+	double y;
+	
+
+	public Point2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	public String getdetails() {
+		return "point(" +x +","+y+")";
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public boolean isEqual(double x,double y) {
+		return this.x==x && this.y==y;
+	}
+	public double CalculateDistance(double x,double y) {
+		double dx=this.x-x;
+		double dy=this.y-y;
+		return Math.sqrt(dx*dx+dy*dy);
+		
+}}
